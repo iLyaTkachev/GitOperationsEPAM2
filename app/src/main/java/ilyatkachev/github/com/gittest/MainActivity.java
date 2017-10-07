73f2f0f4118567788af9f33f43b08068e12d7606
Ilya Tkachev <ilya.tkachev97@gmail.com>
2017-10-07 20:01:38 +0300

    initial setup

---
+++ app/src/main/java/ilyatkachev/github/com/gittest/MainActivity.java
@@ -0,0 +1,15 @@
+package ilyatkachev.github.com.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //line 1 from master
+    }
+}
